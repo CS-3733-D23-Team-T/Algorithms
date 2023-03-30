4dc7e2077aea32aff248ebe735a44e5e9cc2efe9
ybenchikhi <youssefbenchikhi123@gmail.com>
2023-03-29 20:26:19 -0400

    Created algorithm for pathfinding application

--- Main.java
+++ Main.java
@@ -1,2 +1,20 @@
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        int[][] nodes = { /* Your node data here */ };
+        int startNode = 0;
+        int endNode = 1;
+
+        List<Integer> path = aStar.aStar(startNode, endNode, nodes);
+
+        if (path != null) {
+            System.out.println("Shortest path from node " + startNode + " to node " + endNode + ":");
+            for (int node : path) {
+                System.out.print(node + " ");
+            }
+        } else {
+            System.out.println("There is no path from node " + startNode + " to node " + endNode);
+        }
+    }
 }
